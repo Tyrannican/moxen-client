3c9f2456f4ddeac933195e095f0cd187227e5df4
Graham Keenan <graham.keenan@outlook.com>
2024-07-27 01:57:00 +0100

    cli docs

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,11 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
+#[clap(name = "Moxen")]
+#[clap(version = "0.1")]
+#[clap(about = "World of Warcraft Addon development toolkit", long_about = None)]
 pub struct Cli {
+    /// DEBUG ONLY: Sets the directory to a test location
     #[clap(short, long)]
     pub directory: Option<String>,
 
@@ -11,12 +15,21 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    /// Create a new Moxen project
     New {
         /// Name of the new project
         name: String,
     },
+
+    /// Display information about a Moxen project
     Info,
+
+    /// Package a Moxen project ready for publishing
     Package,
+
+    /// Publish a Moxen project to the registry
     Publish,
+
+    /// Clean any packaged artifacts
     Clean,
 }
